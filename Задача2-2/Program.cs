acdc438be94de26c9334646798ef4f9c6b074cf5
Olga <olkasim@inbox.ru>
2022-08-14 13:33:09 +0300

    добавила второй способ решения задачи

---
+++ Задача2-2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("введите первое число");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine ("введите второе число");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine ("введите третье число");
+int C= int.Parse(Console.ReadLine()!);
+
+int max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+
+Console.Write ("max =");
+Console.WriteLine (max);
+
+
