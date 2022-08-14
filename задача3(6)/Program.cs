75727509615e67bbc9f070a8a3698dc31eee96cc
Olga <olkasim@inbox.ru>
2022-08-14 19:16:24 +0300

    решение задачи6

---
+++ задача3(6)/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("введите число");
+int numberA= int.Parse(Console.ReadLine()!);
+if ( numberA % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
