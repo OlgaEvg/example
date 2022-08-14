6aa30571cf4e19eead5af3ac8fb8aab5c022a24b
Olga <olkasim@inbox.ru>
2022-08-14 20:33:17 +0300

    решение задачи 8

---
+++ задача4(8)/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("введите число N");
+int N = int.Parse(Console.ReadLine()!);
+int count = 0;
+while (count < N)
+{
+     count +=2;
+    if (count <= N);
+    Console.WriteLine(count);    
+}
