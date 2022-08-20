8f4db57622e7ad46647c90ed8087ae1b05aaa81d
Olga <olkasim@inbox.ru>
2022-08-20 18:05:43 +0300

    задача 15 семинар2

---
+++ задача№15семинар2/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("введите цифру, соответствующую дню недели:");
+int A = int.Parse(Console.ReadLine()!);
+if (A<6)
+{
+    System.Console.WriteLine("рабочий день");
+}
+if (A>5)
+{
+    System.Console.WriteLine("выходной день");
+}
