5b1dff4b0e33c784c8a938c629441bf53340e6cc
Olga <olkasim@inbox.ru>
2022-08-26 20:07:39 +0300

    решение задачи13

---
+++ задача13семинар2/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine ("введите число:");
+int A= int.Parse(Console.ReadLine()!);
+if (A<100)
+{
+Console.WriteLine ( "третьей цифры нет");
+}
+else
+{
+    while ( A>999 )
+     {
+       A= A / 10;
+     }
+        Console.WriteLine($"третья цифра : { A % 10 } ");
+}
