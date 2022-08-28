1f17aea1bb29da2189fa3be8a84a630565b071c5
Olga <olkasim@inbox.ru>
2022-08-28 15:33:47 +0300

    решение задачи №25

---
+++ задача№25семинар4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Write("введите число А: ");
+int A =int.Parse(Console.ReadLine()!);
+Console.Write("введите число В: ");
+int B =int.Parse(Console.ReadLine()!);
+int stepen=B;
+
+//int AStepenB (int A, int B);
+//count=1;
+//while (count<B);
+//{
+//  A*count;
+//  count++;
+//}
+//Console.WriteLine(AStepenB);
+
+Math.Pow(A, stepen );
+Console.WriteLine("A^B=" + Math.Pow(A, stepen ) );
