936134c539bef543b6381f80ae2faf215c401128
Olga <olkasim@inbox.ru>
2022-09-03 14:30:54 +0300

    решение задачи 19 семинар3

---
+++ задача19семинар3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 
+Console.Write("введите число: ");
+bool palindrome=true;
+int n=int.Parse(Console.ReadLine()!);
+int c =n.ToString().Length - 1;
+string b=n.ToString();
+for (int i=0; i<c; i++)
+   if (b[i] !=b[c-i])
+     palindrome=false;
+     if (palindrome == true)
+     Console.Write ("yes");
+     else Console.Write("No");
+
