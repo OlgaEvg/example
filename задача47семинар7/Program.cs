7f078b2c740beac3024c668665c38fa1f841b259
Olga <olkasim@inbox.ru>
2022-09-12 22:42:57 +0300

    решение задачи47

---
+++ задача47семинар7/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("количество строк в массиве:");
+int str=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("количество столбцов в массиве: ");
+int col=Convert.ToInt32(Console.ReadLine());
+double [,] numbers= new double[str, col];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers (double [,] array)
+{
+    for (int i=0; i< array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            array[i, j]=Convert.ToDouble(new Random().Next(-100, 100))/10;
+        }
+    }
+}
+void PrintArray(double [,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j =0; j< array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+ " ");
+        
+        }
+        Console.Write("]");
+        Console.WriteLine(" ");
+    }
+}
